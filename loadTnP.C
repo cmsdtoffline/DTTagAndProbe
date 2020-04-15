d8ba25bda07a024d1df9d70b09a031191ef7b4e5
Carlo Battilana <carlo.battilana@cern.ch>
2020-04-15 14:57:27 +0200

    First runnin version of DT segment TnP with Run-3 ntuples

--- loadTnP.C
+++ loadTnP.C
@@ -6,7 +6,7 @@ void loadSegmentTnP()
 
   //gSystem->Load("/opt/local/lib/libboost_filesystem-mt.dylib");
   
-  gROOT->ProcessLine(".L DTAnalyzer.C++");
+  gROOT->ProcessLine(".L DTNtupleBaseAnalyzer.C++");
   gROOT->ProcessLine(".L DTTnPConfig.C++");
   gROOT->ProcessLine(".L DTTnPBaseAnalysis.C++");
   gROOT->ProcessLine(".L DTTnPSegmentEff.C++");
